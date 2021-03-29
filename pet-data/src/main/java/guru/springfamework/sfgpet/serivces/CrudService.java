6ef41b9f1d66602e6c90ec63e87d8f4f0d235d7a
mpgis <kuba.slusarski@gmail.com>
2021-03-29 16:20:55 +0200

    refactor to common crud inteface. closes #11

---
+++ pet-data/src/main/java/guru/springfamework/sfgpet/serivces/CrudService.java
@@ -0,0 +1,16 @@
+package guru.springfamework.sfgpet.serivces;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+}
--- pet-data/src/main/java/guru/springfamework/sfgpet/serivces/VetService.java
+++ pet-data/src/main/java/guru/springfamework/sfgpet/serivces/VetService.java
@@ -2,13 +2,6 @@ package guru.springfamework.sfgpet.serivces;
 
 import guru.springfamework.sfgpet.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
