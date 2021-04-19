fb5a038b3185c33e48d6cd6b73149f3780317cfe
mpgis <kuba.slusarski@gmail.com>
2021-04-19 21:53:06 +0200

    Implemented map imp for visit service. Closes #46

---
+++ pet-data/src/main/java/guru/springfamework/sfgpet/serivces/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springfamework.sfgpet.serivces;
+
+import guru.springfamework.sfgpet.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-data/src/main/java/guru/springfamework/sfgpet/serivces/map/VisitServiceMap.java
@@ -0,0 +1,43 @@
+package guru.springfamework.sfgpet.serivces.map;
+
+import guru.springfamework.sfgpet.model.Visit;
+import guru.springfamework.sfgpet.serivces.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+
+        if(object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null
+        || object.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
